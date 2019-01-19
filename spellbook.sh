2ec96c652b2912debe55d7472c042b6925f8a112
Serjpinski <serjpinski@gmail.com>
2019-01-20 00:15:30 +0100

    Test bash function file

--- spellbook.sh
+++ spellbook.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-alias spellreload='source $PWD/book.alias'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+alias spellreload='source ${DIR}/book.alias'
 
 function spell () {
-        python3 $PWD/spellbook.py "$@"
+        python3 ${DIR}/spellbook.py "$@"
         if [[ $? -eq 0 ]]; then
                 [[ $# = 2 ]] && [[ $1 = "remove" ]] && unset -f $2
                 spellreload
