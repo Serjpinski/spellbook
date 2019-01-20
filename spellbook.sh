efd7e7ad12b3ad321b4aeb1401903c5d9ba904ab
Serjpinski <serjpinski@gmail.com>
2019-01-20 01:50:12 +0100

    Rename bar

--- spellbook.sh
+++ spellbook.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SPELLBOOK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-alias spellreload='source ${DIR}/book.alias'
+alias spellreload='source ${SPELLBOOK_DIR}/book.alias'
 
 function spell () {
 
-    python3 ${DIR}/spell.py "$@"
+    python3 ${SPELLBOOK_DIR}/spell.py "$@"
 
     if [[ $? -eq 0 ]]; then
         [[ $# = 2 ]] && [[ $1 = "remove" ]] && unset -f $2
