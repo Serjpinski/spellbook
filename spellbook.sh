0fe7dea1a3e69368e3b45fc6f5ed476acad26002
Serjpinski <serjpinski@gmail.com>
2019-01-20 01:04:25 +0100

    Try python helper script instead

--- spellbook.sh
+++ spellbook.sh
@@ -5,11 +5,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 alias spellreload='source ${DIR}/book.alias'
 
 function spell () {
-        python3 ${DIR}/spellbook.py "$@"
-        if [[ $? -eq 0 ]]; then
-                [[ $# = 2 ]] && [[ $1 = "remove" ]] && unset -f $2
-                spellreload
-        fi
+
+    python3 ${DIR}/spell.py "$@"
+
+    if [[ $? -eq 0 ]]; then
+        [[ $# = 2 ]] && [[ $1 = "remove" ]] && unset -f $2
+        spellreload
+    fi
 }
 
 spellreload
