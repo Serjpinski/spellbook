9ee92c99a72283db986f629177035c847d60dfdf
Sergio Larrodera <serjpinski@gmail.com>
2019-01-21 12:08:16 +0100

    Fix base dir detection

--- spellbook.sh
+++ spellbook.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-SPELLBOOK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BASE_DIR=$(dirname "$0")
 
-alias spellreload='source ${SPELLBOOK_DIR}/book.alias'
+alias spellreload='source ${BASE_DIR}/book.alias'
 
 function spell () {
 
-    python3 ${SPELLBOOK_DIR}/spell.py "$@"
+    python3 ${BASE_DIR}/spell.py "$@"
 
     if [[ $? -eq 0 ]]; then
         [[ $# = 2 ]] && [[ $1 = "remove" ]] && unset -f $2
